53a04eb1475c3a62694aaac3d7d567dadfaa1f25
andrepat0 <70520359+andrepat0@users.noreply.github.com>
2026-09-16 13:18:01 +0000

    Deploying to gh-pages from  @ faad49299e64c346c6eb3397d2af1653ad3f7876 🚀

---
+++ assets/SelectBox.stories-CpRtZDEu.js
@@ -0,0 +1,40 @@
+import{S as n}from"./SelectBox-DoOXpDGP.js";import"./iframe-DSyOqSDN.js";import"./preload-helper-Ct5FWWRu.js";import"./index-B-1qHPVX.js";import"./chevron-down-BfTnk5W5.js";import"./createLucideIcon-BSu-WilA.js";import"./check-BJhEstKV.js";import"./visuallyHidden-CbwUG2x5.js";import"./useRenderElement-D1btaSxw.js";import"./resolveValueLabel-Da30xi4h.js";import"./useControlled-_aVCvc76.js";import"./useOpenChangeComplete-BwHnK81Y.js";import"./index-W9Uhmc6e.js";import"./index-CPWo8sl9.js";import"./useOnMount-B1UIL2E3.js";import"./LabelableContext-CEQz7W2L.js";import"./useLabelableId-DrXdH1K-.js";import"./useBaseUiId-1sq4ghKn.js";import"./useId-Cgn6PTeu.js";import"./useTransitionStatus-DJXo2RaG.js";import"./popupStateMapping-CO8rG5Hk.js";import"./FocusGuard-Cy6It9sP.js";import"./element-CHNw-0cK.js";import"./useTimeout-Drji3j7z.js";import"./event-1uLbwa05.js";import"./index-DQXr03cW.js";import"./useValueChanged-CLp_ZuNQ.js";import"./InternalBackdrop-D0i0FOgA.js";import"./owner-CvMgaIeV.js";import"./getDisabledMountTransitionStyles-Ds7ygwbY.js";import"./floating-ui.utils-FVW-pggS.js";import"./useClick-CcF0LRSa.js";import"./useListNavigation-B4t2d84e.js";import"./composite-CxGP8h6e.js";import"./getPseudoElementBounds-mjS8TYLH.js";import"./useButton-Yc5WbqZJ.js";import"./inertValue-6-tLnjer.js";import"./ToolbarRootContext-DMMrCqel.js";import"./composite-C9OGADUP.js";const Q={title:"Form/SelectBox",component:n,tags:["autodocs"],argTypes:{onChange:{action:"changed"},disabled:{control:"boolean"},error:{control:"boolean"}}},e=[{value:"apple",label:"Apple"},{value:"banana",label:"Banana"},{value:"cherry",label:"Cherry"},{value:"date",label:"Date"},{value:"elderberry",label:"Elderberry"}],r={args:{label:"Favorite Fruit",placeholder:"Pick a fruit",options:e}},o={args:{label:"Favorite Fruit",value:"banana",options:e}},a={args:{label:"Disabled Select",placeholder:"Cannot select",options:e,disabled:!0}},t={args:{label:"Error Select",options:e,error:!0}},l={args:{label:"Long List",options:[...e,{value:"fig",label:"Fig"},{value:"grape",label:"Grape"},{value:"honeydew",label:"Honeydew"}]}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Favorite Fruit',
+    placeholder: 'Pick a fruit',
+    options: options
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Favorite Fruit',
+    value: 'banana',
+    options: options
+  }
+}`,...o.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Disabled Select',
+    placeholder: 'Cannot select',
+    options: options,
+    disabled: true
+  }
+}`,...a.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Error Select',
+    options: options,
+    error: true
+  }
+}`,...t.parameters?.docs?.source}}};l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Long List',
+    options: [...options, {
+      value: 'fig',
+      label: 'Fig'
+    }, {
+      value: 'grape',
+      label: 'Grape'
+    }, {
+      value: 'honeydew',
+      label: 'Honeydew'
+    }]
+  }
+}`,...l.parameters?.docs?.source}}};const R=["Default","Preselected","Disabled","ErrorState","WithManyOptions"];export{r as Default,a as Disabled,t as ErrorState,o as Preselected,l as WithManyOptions,R as __namedExportsOrder,Q as default};
